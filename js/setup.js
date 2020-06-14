505e7e7333e1dc347d0b461702906edadea83669
Bogdan <bogdankuzzmin@gmail.com>
2020-06-15 02:35:25 +0600

    changed the structure of the code

--- js/setup.js
+++ js/setup.js
@@ -7,38 +7,25 @@ var WIZZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '
 var WIZZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var userMenu = document.querySelector('.setup');
-userMenu.classList.remove('hidden');
-
 var getRandomNumber = function (arr) {
   return Math.floor(Math.random() * arr.length);
-};
-
-var getWizzard = function (names, surnames, coatColors, eyesColors) {
-  var wizzardObj = {
-    name: names[getRandomNumber(names)] + ' ' + surnames[getRandomNumber(surnames)],
-    coatColor: coatColors[getRandomNumber(coatColors)],
-    eyesColor: eyesColors[getRandomNumber(eyesColors)]
-  };
-
-  return wizzardObj;
 };
 
 var getWizzards = function (quantity) {
   var wizzardsArr = [];
 
   for (var i = 0; i < quantity; i++) {
-    wizzardsArr.push(getWizzard(WIZZARD_NAMES, WIZZARD_SURNAMES, WIZZARD_COAT_COLORS, WIZZARD_EYES_COLOR));
+    var wizzardObj = {
+      name: WIZZARD_NAMES[getRandomNumber(WIZZARD_NAMES)] + ' ' + WIZZARD_SURNAMES[getRandomNumber(WIZZARD_SURNAMES)],
+      coatColor: WIZZARD_EYES_COLOR[getRandomNumber(WIZZARD_EYES_COLOR)],
+      eyesColor: WIZZARD_COAT_COLORS[getRandomNumber(WIZZARD_COAT_COLORS)]
+    };
+
+    wizzardsArr.push(wizzardObj);
   }
 
   return wizzardsArr;
 };
-
-var wizzards = getWizzards(WIZZARDS_NUMBER);
-
-var wizzardTemplate = document.getElementById('similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
 
 var renderWizzard = function (wizzard) {
   var wizzardElement = wizzardTemplate.cloneNode(true);
@@ -61,8 +48,16 @@ var addWizzards = function (character) {
   userMenuWizzardsList.appendChild(fragment);
 };
 
-addWizzards(wizzards);
+var wizzardTemplate = document.getElementById('similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
 
+var userMenu = document.querySelector('.setup');
+userMenu.classList.remove('hidden');
 
 var userMenuWizzards = document.querySelector('.setup-similar');
 userMenuWizzards.classList.remove('hidden');
+
+var wizzards = getWizzards(WIZZARDS_NUMBER);
+
+addWizzards(wizzards);
