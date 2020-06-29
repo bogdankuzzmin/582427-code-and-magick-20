1fe0229ce4e74543ac45ea33250ec2b36459dd72
Bogdan <bogdankuzzmin@gmail.com>
2020-06-30 05:47:27 +0600

    moved rgbToHex from wizard.js

--- js/colorize.js
+++ js/colorize.js
@@ -15,7 +15,30 @@
     }
   };
 
+  var rgbToHex = function (rgb) {
+    var sep = rgb.indexOf(',') > -1 ? ',' : '';
+
+    rgb = rgb.substr(4).split(')')[0].split(sep);
+
+    var r = (+rgb[0]).toString(16);
+    var g = (+rgb[1]).toString(16);
+    var b = (+rgb[2]).toString(16);
+
+    if (r.length === 1) {
+      r = '0' + r;
+    }
+    if (g.length === 1) {
+      g = '0' + g;
+    }
+    if (b.length === 1) {
+      b = '0' + b;
+    }
+
+    return '#' + r + g + b;
+  };
+
   window.colorize = {
-    colorize: colorize
+    colorize: colorize,
+    rgbToHex: rgbToHex
   };
 })();
