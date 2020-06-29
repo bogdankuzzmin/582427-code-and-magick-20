a6db902aac8f101c5ce20e78704a9556cbf0d863
Bogdan <bogdankuzzmin@gmail.com>
2020-06-30 05:49:39 +0600

    added debounce

--- js/wizard.js
+++ js/wizard.js
@@ -26,28 +26,6 @@
     '#e6e848'
   ];
 
-  var rgbToHex = function (rgb) {
-    var sep = rgb.indexOf(',') > -1 ? ',' : '';
-
-    rgb = rgb.substr(4).split(')')[0].split(sep);
-
-    var r = (+rgb[0]).toString(16);
-    var g = (+rgb[1]).toString(16);
-    var b = (+rgb[2]).toString(16);
-
-    if (r.length === 1) {
-      r = '0' + r;
-    }
-    if (g.length === 1) {
-      g = '0' + g;
-    }
-    if (b.length === 1) {
-      b = '0' + b;
-    }
-
-    return '#' + r + g + b;
-  };
-
   var coatChangeHandler = window.debounce(function (color) {
     window.filter.coatColor = color;
     window.filter.updateWizards();
@@ -69,7 +47,7 @@
       eyesChangeHandler(window.colorize.newColor);
     } else if (evt.target.matches('.setup-fireball')) {
       window.colorize.colorize(fireball, WIZARD_FIREBALL_COLOR);
-      wizardFireballColorInput.value = rgbToHex(evt.target.style.backgroundColor);
+      wizardFireballColorInput.value = window.colorize.rgbToHex(evt.target.style.backgroundColor);
     }
   };
 
